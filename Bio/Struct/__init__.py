1a266aa295d6d0b94f5df36635cba392dc259298
Joao Rodrigues <anaryin@gmail.com>
2011-05-05 22:00:08 +0200

    Added read method to Bio.Struct __init__.py

--- Bio/Struct/__init__.py
+++ Bio/Struct/__init__.py
@@ -4,3 +4,34 @@
 # as part of this package.
 
 """High-level wrapper and extension for Bio.PDB"""
+
+import os
+
+from Bio.PDB import PDBParser, MMCIFParser
+from Bio.PDB import PDBIO
+
+def read(fpath, format=None, PERMISSIVE=1):
+    """
+    Wrapper for Bio.PDB parsers.
+    
+    Arguments:
+      fpath         - path to file
+      format        - file format (pdb, mmcif)
+      PERMISSIVE    - PERMISSIVE flag (for PDBParser only)
+    """
+    
+    # Get format
+    if not format or format.lower() == 'pdb':
+        parser = PDBParser(PERMISSIVE=PERMISSIVE)
+    elif format.lower() == 'mmcif':
+        parser = MMCIFParser()
+    else:
+        raise KeyError("Unrecognized format: %s" %format)
+    
+    # Parse structure name from path
+    name = os.path.basename(fpath).split('.')[-1]
+    
+    # Parse structure
+    structure = parser.get_structure(name, fpath)
+    
+    return structure
