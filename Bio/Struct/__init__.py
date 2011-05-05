6a2238438360d0dc2611e5aa4e5408ef386f4199
Joao Rodrigues <anaryin@gmail.com>
2011-05-05 21:59:24 +0200

    Added Bio/Struct

---
+++ Bio/Struct/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2011, Joao Rodrigues (j.rodrigues@uu.nl)
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""High-level wrapper and extension for Bio.PDB"""
