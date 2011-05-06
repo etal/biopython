0d9ea74346251359104f7065a57e215673ed315f
Joao Rodrigues <anaryin@gmail.com>
2011-05-06 09:41:16 +0200

    Added SeqIO-like read() to Bio.Struct. Detached I/O methods to Bio/Struct/_io.py

--- Bio/Struct/__init__.py
+++ Bio/Struct/__init__.py
@@ -1,70 +1,13 @@
-# Copyright (C) 2011, Joao Rodrigues (j.rodrigues@uu.nl)
+# Copyright (C) 2011 by Joao Rodrigues (j.rodrigues@uu.nl)
 # This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
+# license. Please see the LICENSE file that should have been included
 # as part of this package.
 
-"""High-level wrapper and extension for Bio.PDB"""
+"""Package for Structural Biology Routines .
 
-import os, warnings
+See also: U{ http://biopython.org/wiki/Struct }
+"""
 
-from Bio.PDB import PDBParser
-try:
-    from Bio.PDB import MMCIFParser
-    has_mmcif = True
-except ImportError:
-    has_mmcif = False
-    print "No module named MMCIFlex.\n" \
-          "You will not be able to parse MMCIF files.\n" \
-          "Please check the FAQ for details on the installation."
+__docformat__ = "epytext en"
 
-from Bio.PDB import PDBIO
-
-def read(fpath, name=None, format=None, PERMISSIVE=1, quiet=False):
-    """
-    Wrapper for Bio.PDB parsers.
-    
-    Mandatory Arguments:
-      fpath         - path to file
-
-    Optional Arguments:
-      name          - structure name (for Structure Object) [default: file name]
-      format        - file format (pdb, mmcif) [default: PDB]
-      PERMISSIVE    - PERMISSIVE flag (for PDBParser only) [default: 1]
-      quiet         - Supresses warning messages while parsing [default: False]
-    """
-    
-    # Allowed Formats
-    allowed_formats = { 'pdb': PDBParser() }
-    
-    if has_mmcif:
-        allowed_formats['mmcif'] = MMCIFParser()
-    
-    # Get format
-    if not format:
-        # Guess from extension or should it default to PDB?
-        ext = fpath.split('.')[-1]
-        format = ext
-
-    format= format.lower()
-    if format in allowed_formats:
-        parser = allowed_formats[format]
-    else:
-        raise KeyError("Unrecognized format: %s" %format)
-    
-    # Parse structure name from path if not given
-    if not name:
-        ext = fpath.split('.')[-1]
-        name = os.path.basename(fpath)[:-len(ext)-1]
-    
-    # Supress warnings
-    if quiet:
-        warnings.simplefilter('ignore')
-
-    # Parse structure
-    structure = parser.get_structure(name, fpath)
-
-    # Reset warnings
-    if quiet:
-        warnings.resetwarnings()
-
-    return structure
+from Bio.Struct._io import read#, write, convert
---
+++ Bio/Struct/_io.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2011, Joao Rodrigues (j.rodrigues@uu.nl)
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""I/O function wrapper and for structural file formats.
+
+This API follows the same semantics as Biopython's SeqIO and AlignIO.
+"""
+
+import os, warnings
+
+from Bio.PDB import PDBIO
+from Bio.PDB import PDBParser
+
+supported_formats = {
+        'pdb': PDBParser 
+        }
+
+# Flex is an optional dependency
+try:
+    from Bio.PDB import MMCIFParser
+except ImportError:
+    warnings.warn("Missing module MMCIFlex: MMCIF files unsupported.")
+else:
+    supported_formats['mmcif'] = MMCIFParser 
+
+# Public Functions
+def read(infile, format='pdb', id=None, **kwargs):
+    """
+    Wrapper for Bio.PDB parsers.
+    
+    Mandatory Arguments:
+        file    - Path to File or File Handle
+
+    Optional Arguments:
+        format  - File Format (e.g. 'pdb')
+        sid     - Structure Id to pass to Parser
+    
+    Other parser specific arguments can be passed as
+    keyworded arguments.
+    """
+    
+    # Get format and instanciate
+    p = supported_formats[format.lower()]
+    Parser = p(**kwargs)
+    
+    if not id: # Take file name without the extension
+        if not isinstance(infile, basestring):
+            infile_name = infile.name
+        else:
+            infile_name = infile
+        ext = infile_name.split('.')[-1]
+        id = os.path.basename(infile_name)[:-len(ext)-1]
+
+    structure = getattr(Parser, 'get_structure')(id, infile)
+
+    return structure
