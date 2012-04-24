072667e501518c0bf1179a0e36caaf019c6ff5cd
João Rodrigues <anaryin@gmail.com>
2012-04-24 16:10:51 +0200

    Created Struct/StructAnalysis.py

---
+++ Bio/Struct/StructAnalysis.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2012, Joao Rodrigues (anaryin@gmail.com)
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""
+Assorted utility functions to manipulate structural data.
+
+This module serves as a container for all structural manipulation
+utilities that used to belong in the structure objects themselves.
+
+Functions should take as argument a collection of objects, or those
+that can be unfolded in children (Structure, Chain, ...)
+The output should be an equivalent data structure.
+
+The module should also be callable directly to allow straightforward
+manipulation in a shell enviroment.
+
+Available functions
+
+None
+"""
+
+
+if __name__ == '__main__':
+  pass
+     
